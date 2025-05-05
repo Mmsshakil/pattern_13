9aaf851e0ce1c1b72a04873d9f2f09ad4b9dd704
Mmsshakil <softhandshakil@gmail.com>
2025-05-05 22:36:20 +0600

    another way of digit pattern

--- digit_pattern.c
+++ digit_pattern.c
@@ -3,20 +3,21 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int space = n - 1;
+    int space = n - 1, value = 1;
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) // for line
     {
-        for (int k = space; k >= 1; k--)
+        for (int k = space; k >= 1; k--) // for space
         {
             printf(" ");
         }
 
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= value; j++) // for digit
         {
             printf("%d", j);
         }
         printf("\n");
+        value++;
         space--;
     }
 
